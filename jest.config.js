a8669a89b0284f4444c891ec40dddd9cea8a0ae4
Chris Sauve <chrismsauve@gmail.com>
2019-04-19 13:57:07 -0400

    Move a few things around

--- jest.config.js
+++ jest.config.js
@@ -6,13 +6,13 @@ module.exports = {
   },
   coverageDirectory: './coverage/',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ],
   moduleNameMapper: {
     '^@shopify/useful-types': '<rootDir>/node_modules/@shopify/useful-types',
     '^@shopify/([a-zA-Z0-9_-]+)(/[a-zA-Z0-9_-]+)$': '<rootDir>/packages/$1/src/$2',
     '^@shopify/([a-zA-Z0-9_-]+)$': '<rootDir>/packages/$1',
   },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
